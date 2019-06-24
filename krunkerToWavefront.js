c5fd5e00539da6d17c06ed5fe33ea062c47a955e
CobaltXII <sidatt64@gmail.com>
2019-06-24 14:49:59 -0400

    Update krunkerToWavefront.js

--- krunkerToWavefront.js
+++ krunkerToWavefront.js
@@ -11,3 +11,16 @@ function generateCubeVertexInfo(cube, vertexCount, faceCount) {
 	info += "v " + (cube.p[0] + cube.s[0]) + " " + (cube.p[1] + cube.s[1]) + " " + (cube.p[2] + cube.s[2]) + "\n";
 	return info;
 }
+
+// Generate the face information for a cube.
+function generateCubeFaceInfo(cube, vertexCount, faceCount) {
+	var info = "";
+	vertexCount++;
+	info += "f " + (vertexCount + 0) + " " + (vertexCount + 2) + " " + (vertexCount + 4) + " " + (vertexCount + 1) + "\n";
+	info += "f " + (vertexCount + 1) + " " + (vertexCount + 4) + " " + (vertexCount + 7) + " " + (vertexCount + 5) + "\n";
+	info += "f " + (vertexCount + 5) + " " + (vertexCount + 7) + " " + (vertexCount + 6) + " " + (vertexCount + 3) + "\n";
+	info += "f " + (vertexCount + 3) + " " + (vertexCount + 6) + " " + (vertexCount + 2) + " " + (vertexCount + 0) + "\n";
+	info += "f " + (vertexCount + 2) + " " + (vertexCount + 6) + " " + (vertexCount + 7) + " " + (vertexCount + 4) + "\n";
+	info += "f " + (vertexCount + 3) + " " + (vertexCount + 0) + " " + (vertexCount + 1) + " " + (vertexCount + 5) + "\n";
+	return info;
+}
